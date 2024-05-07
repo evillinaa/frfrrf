43ccb2cc3e8629e8584ba49ce5875fdf2f3d97f9
Igor Tyshchenko <igor@tyshchenko.me>
2024-05-07 16:03:07 +0500

    Добавьте файлы проекта.

---
+++ Choice.cpp
@@ -0,0 +1,74 @@
+﻿#include "Choice.hpp"
+#include "Functions.hpp"
+#include <iostream>
+#include <Windows.h>
+#include "Functions.h"
+
+
+
+int Choice()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    const int size = 10;
+    float array[size]{};
+
+    int choice;
+    int sortChoice;
+
+    while (true) {
+        system("cls");
+
+        std::cout << "Программа для работы с массивом" << "\n";
+        std::cout << "Выберите способ ввода массива:" << "\n";
+        std::cout << "1. Ввести вручную" << "\n";
+        std::cout << "2. Заполнить автоматически" << "\n";
+        std::cout << "0. Выйти из программы" << "\n";
+        std::cin >> choice;
+
+        if (choice == 1) {
+            manualInput(array, size);
+        }
+        else if (choice == 2) {
+            autoInput(array, size);
+        }
+        else if (choice == 0) {
+            break;
+        }
+        else {
+            std::cout << "Некорректный выбор. Попробуйте снова." << "\n";
+            continue;
+        }
+
+        system("cls");
+
+        std::cout << "������:" << std::endl;
+        printArray(array, size);
+
+        std::cout << "Выберите способ сортировки:\n1) По убыванию\n2) По возрастанию\n3) Не изменять массив\n";
+        std::cin >> sortChoice;
+
+        switch (sortChoice) {
+        case 1:
+            bubbleSortDescending(array, size);
+            printArray(array, size);
+            break;
+        case 2:
+            bubbleSortAscending(array, size);
+            printArray(array, size);
+            break;
+        case 3:
+            std::cout << "Массив остается без изменений." << "\n";
+            printArray(array, size);
+            break;
+        default:
+            std::cout << "Некорректный выбор сортировки." << "\n";
+            break;
+        }
+
+        system("pause");
+    }
+
+    return 0;
+}
---
+++ frfrrf.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "Choice.hpp"
+
+
+int main() {
+
+	Choice();
+
+	system("pause");
+	return 0;
+}
